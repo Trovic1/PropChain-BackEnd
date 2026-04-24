869b8208a8ba38171329f8e0b5ea3ef6eb0ba241
phertyameen <aminubabafatima8@gmail.com>
2026-04-24 19:34:21 +0100

    fix: ci

--- src/content/content.module.ts
+++ src/content/content.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ContentService } from './content.service';
 import { ContentController } from './content.controller';
-import { PrismaService } from '../prisma/prisma.service';
-
+import { PrismaService } from 'src/database/prisma.service';
 @Module({
   providers: [ContentService, PrismaService],
   controllers: [ContentController],
